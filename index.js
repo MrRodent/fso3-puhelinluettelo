c9ee23a0272c3755b99318f3883018a86b338c3a
Tommi <83519567+MrRodent@users.noreply.github.com>
2024-04-17 16:09:17 +0300

    puhelinluettelon backend step2

--- index.js
+++ index.js
@@ -32,9 +32,13 @@ app.get('/api/persons', (req, res) => {
   res.json(persons)
 })
 
+app.get('/info', (req, res) => {
+  const numberOfPeople = Object.keys(persons).length
+  const date = new Date()
+  res.send(`<p>Phonebook has info for ${numberOfPeople} people <hr>${date}</p>`)
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
-
-console.log('test')
