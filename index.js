c544144dfc644f5056aa6979222d9d96c8d68ff0
Tommi <83519567+MrRodent@users.noreply.github.com>
2024-04-24 19:53:02 +0300

    puhelinluettelon backend step9

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
+
 const app = express()
 
 app.use(express.json())
 app.use(morgan('tiny'))
+app.use(cors())
 
 // Custom token joka näyttää pyynnön bodyn (käytetään POSTissa)
 morgan.token('body', (req) => JSON.stringify(req.body))
